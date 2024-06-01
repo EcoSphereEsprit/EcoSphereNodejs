e98593ea9d5fe400c6686ede2bb6ce05eaad3065
Ali Dhifallah <adhifallah@NA.LOCAL>
2024-06-01 12:16:20 +0100

    Update

--- EcoSphere_Back-end/controllers/blog.controller.js
+++ EcoSphere_Back-end/controllers/blog.controller.js
@@ -1,116 +1,76 @@
-import express from 'express';
+
 import Blog from '../models/blog.model.js';
-import multer from 'multer';
 
-
-// Configuration de Multer pour le stockage des images
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
-});
-
-const upload = multer({ storage });
-
-// Create a new blog with image upload
 export const createBlog = async (req, res) => {
     try {
-        const blogData = {
-            title: req.body.title,
-            description: req.body.description,
-            date: req.body.date,
-            image: req.file ? req.file.path : null,
-        };
-        const blog = new Blog(blogData);
+        const { title, description, date, userID } = req.body;
+        const image = req.file.path; // Chemin d'accès temporaire de l'image téléchargée
+
+        // Utilisez userID comme vous le souhaitez, par exemple, pour associer le blog à l'utilisateur
+        console.log(userID);
+
+        const blog = new Blog({ title, description, date, image, user: userID });
         await blog.save();
-        res.status(201).send(blog);
+
+        res.status(201).json(blog);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).json({ error: error.message });
+  
     }
 };
 
-// Obtenir tous les blogs
 export const getAllBlogs = async (req, res) => {
     try {
-        const blogs = await Blog.find().populate('comments');
-        res.send(blogs);
+        const blogs = await Blog.find();
+        res.status(200).json(blogs);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(400).json({ error: error.message });
     }
 };
 
-// Obtenir un blog par ID
 export const getBlogById = async (req, res) => {
     try {
-        const blog = await Blog.findById(req.params.id).populate('comments');
-        if (!blog) {
-            return res.status(404).send();
+        const blog = await Blog.findById(req.params.id);
+        if (blog) {
+            res.status(200).json(blog);
+        } else {
+            res.status(404).json({ error: 'Blog not found' });
         }
-        res.send(blog);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(400).json({ error: error.message });
     }
 };
 
-// Mettre à jour un blog par ID
 export const updateBlog = async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['title', 'description', 'date', 'comments'];
-    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' });
-    }
-    //Ajouter 28/05
-    console.log(`Updating blog with ID: ${id}`);
-    console.log('Request body:', req.body);
-    //fin ajout
-    const updateData = req.body;
     try {
-        const blog = await Blog.findByIdAndUpdate(req.params.id);
-        // Si la date est fournie, mettez à jour la date du blog
-        //Ajout 28/05
-        if (updateData.date) {
-            updateData.date = new Date(updateData.date); // Convertir la date en objet Date
-        }
-
-        // Si une image est téléchargée, mettez à jour le chemin de l'image
+        const { title, description, date } = req.body;
+        let image = req.body.image;
         if (req.file) {
-            updateData.image = req.file.path;
+            // Si une nouvelle image est téléchargée, utilisez son chemin d'accès temporaire
+            image = req.file.path;
         }
-        //fin ajout
-        if (!blog) {
-            console.log('Blog not found');
-            return res.status(404).json({ message: 'Blog not found' });
+        const blog = await Blog.findByIdAndUpdate(req.params.id, { title, description, date, image }, { new: true });
+        if (blog) {
+            res.status(200).json(blog);
+        } else {
+            res.status(404).json({ error: 'Blog not found' });
         }
-        console.log('Blog updated successfully:', blog);
-        res.status(200).json(blog);
-        updates.forEach(update => blog[update] = req.body[update]);
-        if (req.file) {
-            blog.image = req.file.path;
-        }
-        await blog.save();
-        res.send(blog);
     } catch (error) {
-        console.error('Error updating blog:', error);
-        res.status(500).json({ message: error.message });
+        res.status(400).json({ error: error.message });
     }
 };
 
-// Supprimer un blog par ID
+ 
+
 export const deleteBlog = async (req, res) => {
     try {
         const blog = await Blog.findByIdAndDelete(req.params.id);
-
-        if (!blog) {
-            return res.status(404).json({ message: 'Blog not found' });
+        if (blog) {
+            res.status(204).json();
+        } else {
+            res.status(404).json({ error: 'Blog not found' });
         }
-        res.status(200).json({ message: 'Blog deleted successfully' });
-        res.send(blog);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(400).json({ error: error.message });
     }
 };
--- EcoSphere_Back-end/controllers/comment.controller.js
+++ EcoSphere_Back-end/controllers/comment.controller.js
@@ -1,82 +1,87 @@
-import express from 'express';
-import Comment from '../models/comment.model.js';
-import { updateBlog } from '../controllers/blog.controller.js';
 
-// Créer un nouveau commentaire
+
+import Blog from '../models/blog.model.js';
+import Comment from '../models/comment.model.js';
+
 export const createComment = async (req, res) => {
     try {
+        const { content, date } = req.body;
+        const { blogId } = req.params; // Identifiant du blog auquel le commentaire est associé
 
+        // Récupérer le blog auquel ajouter le commentaire
+        const blog = await Blog.findById(blogId);
+        if (!blog) {
+            return res.status(404).json({ error: 'Blog not found' });
+        }
 
-        const comment = new Comment(req.body);
-        console.log(req.body);
+        // Créer le commentaire
+        const comment = new Comment({ content, date, blog: blogId });
         await comment.save();
-        // await updateBlog({body: { comments: { comment }  }, params:{id: req.body.blog}});
-        res.status(201).send(comment);
+
+        // Ajouter le commentaire à la liste des commentaires du blog
+        blog.comments.push(comment);
+        await blog.save();
+
+        res.status(201).json(comment);
     } catch (error) {
-        console.log(error);
-        res.status(400).send(error);
+        res.status(400).json({ error: error.message });
     }
 };
-// Obtenir tous les commentaires
+ 
 export const getAllComments = async (req, res) => {
     try {
-        const comments = await Comment.find().populate('user blog');
-        res.send(comments);
+        const comments = await Comment.find();
+        res.status(200).json(comments);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(400).json({ error: error.message });
     }
 };
 
-// Obtenir un commentaire par ID
 export const getCommentById = async (req, res) => {
     try {
-        const comment = await Comment.findById(req.params.id).populate('user blog');
-        if (!comment) {
-            return res.status(404).send();
+        const comment = await Comment.findById(req.params.id).populate('blog');
+        if (comment) {
+            res.status(200).json(comment);
+        } else {
+            res.status(404).json({ error: 'Comment not found' });
         }
-        res.send(comment);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(400).json({ error: error.message });
     }
 };
 
-// Mettre à jour un commentaire par ID
 export const updateComment = async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['content', 'date', 'blog', 'user'];
-    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' });
-    }
-
     try {
-        const comment = await Comment.findById(req.params.id);
-
-        if (!comment) {
-            return res.status(404).send();
+        const comment = await Comment.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (comment) {
+            res.status(200).json(comment);
+        } else {
+            res.status(404).json({ error: 'Comment not found' });
         }
-
-        updates.forEach(update => comment[update] = req.body[update]);
-        await comment.save();
-        res.send(comment);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).json({ error: error.message });
     }
 };
 
-// Supprimer un commentaire par ID
 export const deleteComment = async (req, res) => {
     try {
         const comment = await Comment.findByIdAndDelete(req.params.id);
-
-        if (!comment) {
-            return res.status(404).json({ message: 'Comment not found' });
-
+        if (comment) {
+            // Trouver le blog associé au commentaire
+            const blog = await Blog.findOne({ comments: req.params.id });
+            if (blog) {
+                // Supprimer l'identifiant du commentaire de la liste des commentaires du blog
+                blog.comments.pull(req.params.id);
+                await blog.save();
+            }
+            res.status(204).json();
+        } else {
+            res.status(404).json({ error: 'Comment not found' });
         }
-        res.status(200).json({ message: 'Comment deleted successfully' });
-        res.send(comment);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(400).json({ error: error.message });
     }
 };
+
+
+ 
