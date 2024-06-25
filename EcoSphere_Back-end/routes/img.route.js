4fa1c2de630b004a51437473a566fb53a0d353de
Ali Dhifallah <adhifallah@NA.LOCAL>
2024-06-25 21:14:18 +0100

    commit

--- EcoSphere_Back-end/routes/img.route.js
+++ EcoSphere_Back-end/routes/img.route.js
@@ -8,7 +8,7 @@ router
         const imageName = req.params.imageName;
 
         const __dirname = dirname(fileURLToPath(import.meta.url));
-        const imagesFolderPath = join(__dirname, '../public/images' + '\\' + imageName)
+        const imagesFolderPath = join(__dirname, '../public/images' + '/' + imageName)
 
         res.sendFile(imagesFolderPath);
 
